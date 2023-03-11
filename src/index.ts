92e7e93f1eb648c56e5d25f917b3130cad936c0b
Sarah Dellysse <me@sqlazer.com>
2023-03-10 22:43:10 -0800

    removed mqtt support

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,18 @@
 import winston from "winston";
 import { z } from "zod";
 import * as modbus from "./modbus";
-import * as mqtt from "./mqtt";
 import * as http from "./http";
 
 type OptionsDefaultExport = {
-  http?: {
+  http: {
     port: number;
   };
   logger: winston.Logger;
   modbus: {
     uri: string;
   };
-  mqtt?: {
-    prefix: string;
-    uri: string;
-  };
 };
-const defaultExport = async ({logger, ...options}: OptionsDefaultExport) => {
+const defaultExport = async ({ logger, ...options }: OptionsDefaultExport) => {
   logger.debug("options", options);
 
   logger.debug(`connection to modbus via ${options.modbus.uri}`);
@@ -28,107 +23,46 @@ const defaultExport = async ({logger, ...options}: OptionsDefaultExport) => {
   logger.info(`connected to modbus via ${options.modbus.uri}`);
   const useConn = modbus.interlockConn(modbusConn);
 
-  if (options.http !== undefined) {
-    logger.debug(`starting http server on port ${options.http.port}`);
-    const httpServer = await http.listen(options.http.port);
-    logger.info(`started http server on port ${options.http.port}`);
+  logger.debug(`starting http server on port ${options.http.port}`);
+  const httpServer = await http.listen(options.http.port);
+  logger.info(`started http server on port ${options.http.port}`);
 
-    httpServer.post("/read_register", async (req, res) => {
-      logger.debug(`http: POST /read_register: ${JSON.stringify(req.body)}`);
+  httpServer.post("/read_register", async (req, res) => {
+    logger.debug(`http: POST /read_register: ${JSON.stringify(req.body)}`);
 
-      const body = z
-        .object({
-          address: z.number(),
-          register: z.number(),
-          length: z.number(),
-        })
-        .parse(req.body);
+    const body = z
+      .object({
+        address: z.number(),
+        register: z.number(),
+        length: z.number(),
+      })
+      .parse(req.body);
 
-      const bytes = await useConn(
-        logger,
-        async (conn) =>
-          await modbus.readRegister({
-            ...body,
-            conn,
-            logger,
-          })
-      );
-
-      res.status(200);
-      res.header("Content-Type", "application/octet-stream");
-      res.send(bytes);
-      res.end();
-    });
-  }
-
-  if (options.mqtt !== undefined) {
-    const mqttConn = await mqtt.connect({
+    const bytes = await useConn(
       logger,
-      prefix: options.mqtt.prefix,
-      uri: options.mqtt.uri,
-    });
+      async (conn) =>
+        await modbus.readRegister({
+          ...body,
+          conn,
+          logger,
+        })
+    );
 
-    mqttConn.on("message", async (topic, payload, packet) => {
-      if (topic === `${options.mqtt?.prefix}/request.read_register`) {
-        const responseTopic = z
-          .string()
-          .parse(packet.properties?.responseTopic);
-        const message = z
-          .object({
-            address: z.number(),
-            register: z.number(),
-            length: z.number(),
-          })
-          .parse(JSON.parse(payload.toString("utf-8")));
-
-        const bytes = await useConn(logger, async (conn) =>
-          modbus.readRegister({
-            ...message,
-            conn,
-            logger,
-          })
-        );
-
-        await mqttConn.publish(responseTopic, bytes, {
-          properties: {
-            contentType: "application/octet-stream",
-            correlationData:
-              packet.properties?.correlationData ?? Buffer.from(""),
-          },
-          qos: 2,
-        });
-
-        return;
-      }
-    });
-
-    await mqttConn.subscribe([`${options.mqtt.prefix}/request.read_register`]);
-  }
+    res.status(200);
+    res.header("Content-Type", "application/octet-stream");
+    res.send(bytes);
+    res.end();
+  });
 };
 export default defaultExport;
 
 if (require.main === module) {
   (async () => {
-    
-    const envSchema = (() => {
-      const common = z.object({
-        LOGLEVEL: z.string().default("info"),
-        HTTP_PORT: z.string().default("5280").transform(Number).pipe(z.number()),
-        MODBUS_URI: z.string(),
-      });
-
-      const mqttProvided = z.object({
-        MQTT_URI: z.string().url(),
-        MQTT_PREFIX: z.string().default("modbus"),
-      });
-
-      const mqttOmitted = z.object({
-        MQTT_URI: z.undefined(),
-        MQTT_PREFIX: z.undefined(),
-      });
-
-      return z.union([common.merge(mqttProvided), common.merge(mqttOmitted)]);
-    })();
+    const envSchema = z.object({
+      LOGLEVEL: z.string().default("info"),
+      HTTP_PORT: z.string().default("5280").transform(Number).pipe(z.number()),
+      MODBUS_URI: z.string(),
+    });
 
     const env = envSchema.parse(process.env);
 
@@ -143,21 +77,9 @@ if (require.main === module) {
       modbus: {
         uri: env.MODBUS_URI,
       },
-      ...(env.HTTP_PORT !== undefined
-        ? {
-            http: {
-              port: env.HTTP_PORT,
-            },
-          }
-        : {}),
-      ...(env.MQTT_URI !== undefined
-        ? {
-            mqtt: {
-              prefix: env.MQTT_PREFIX,
-              uri: env.MQTT_URI,
-            },
-          }
-        : {}),
+      http: {
+        port: env.HTTP_PORT,
+      },
     });
   })().catch((error) => {
     console.log(error);
